a57677ce359f99cd2467538dca685fd92b292b1b
Gautam Bubna <gautambubna100@gmail.com>
2022-07-20 00:58:47 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import Header from './components/Header';
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+// import PunkList from './components/PunkList';
+import CollectionCard from './components/CollectionCard';
+// import { useNFTCollection } from '@thirdweb-dev/react'
+
+
+
+
+function App () {
+  // const [punkListData, setPunkListData] = useState([])
+  //  useEffect(() => {
+  //     const getMyNfts = async () => {
+  //       const openseaData = await axios.get(
+  //         'https://testnets-api.opensea.io/assets?asset_contract_address=0x219184B104702C19e01290b30Ce07D362a89732e'
+  //       )
+  //       console.log(openseaData.data.assets)
+  //       setPunkListData(openseaData.data.assets)
+  //     }
+
+  //       return getMyNfts()
+  //  }, [])
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <div className='app'>
+     <Header />
+    <CollectionCard id={0}
+     name={'Bandana Punk'}
+      traits={[{value:7}]} 
+    image='https://avatars.githubusercontent.com/u/103533980?v=4' />  
+     
+     </div>
+  )
 }
 
 export default App;
