cb1b4393c7e4f2a5f6a2f0b618de66353ec9c8ce
gautambubna <103533980+gautambubna@users.noreply.github.com>
2022-07-23 15:41:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,26 +3,26 @@ import './App.css';
 import Header from './components/Header';
 import {useState, useEffect} from 'react'
 import axios from 'axios'
-// import PunkList from './components/PunkList';
+import PunkList from './components/PunkList';
 import CollectionCard from './components/CollectionCard';
-// import { useNFTCollection } from '@thirdweb-dev/react'
+import { useNFTCollection } from '@thirdweb-dev/react'
 
 
 
 
 function App () {
-  // const [punkListData, setPunkListData] = useState([])
-  //  useEffect(() => {
-  //     const getMyNfts = async () => {
-  //       const openseaData = await axios.get(
-  //         'https://testnets-api.opensea.io/assets?asset_contract_address=0x219184B104702C19e01290b30Ce07D362a89732e'
-  //       )
-  //       console.log(openseaData.data.assets)
-  //       setPunkListData(openseaData.data.assets)
-  //     }
+  const [punkListData, setPunkListData] = useState([])
+   useEffect(() => {
+      const getMyNfts = async () => {
+        const openseaData = await axios.get(
+          'https://testnets-api.opensea.io/assets?asset_contract_address=0x219184B104702C19e01290b30Ce07D362a89732e'
+        )
+        console.log(openseaData.data.assets)
+        setPunkListData(openseaData.data.assets)
+      }
 
-  //       return getMyNfts()
-  //  }, [])
+        return getMyNfts()
+   }, [])
   
   
   return (
